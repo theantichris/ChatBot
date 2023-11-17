d7711b15fdcf2981cb58d7d4c382abe0e8038f82
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-17 10:12:17 -0500

    adding the questions and answers to an array, displaying the appropriate message to the user

--- ChatBot/ConversationDataSource.swift
+++ ChatBot/ConversationDataSource.swift
@@ -4,9 +4,15 @@ class ConversationDataSource {
     /// The number of Messages in the conversation
     var messageCount = 0
     
+    // Holds the questions and answers in the conversation
+    var messages = [Message]()
+    
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
         
         messageCount += 1
     }
@@ -15,6 +21,9 @@ class ConversationDataSource {
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
         
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
+        
         messageCount += 1
     }
     
@@ -22,10 +31,6 @@ class ConversationDataSource {
     func messageAt(index: Int) -> Message {
         print("Asked for message at index \(index)")
         
-        if index % 2 == 0 {
-            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
-        } else {
-            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
-        }
+        return messages[index]
     }
 }
